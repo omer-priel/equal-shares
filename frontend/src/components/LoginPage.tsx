60727f4564294228f496fbf25bb410dfc04ea072
Omer Priel <omer.priel2@gmail.com>
2024-03-21 15:07:49 +0200

    working on page ui

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 import { Page, UserData } from "../types";
+
+import logoImage from '../assets/logo.png';
 
 export type Props = {
     setCurrentPage: (page: Page) => void;
@@ -6,7 +10,14 @@ export type Props = {
 };
 
 export default function LoginPage({ setCurrentPage, setUser }: Props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [message, setMessage] = useState('');
+
     const loginOnClick = () => {
+        setMessage('Login failed');
+
         setUser({
             token: 'token',
             email: 'example@test.com',
@@ -17,11 +28,47 @@ export default function LoginPage({ setCurrentPage, setUser }: Props) {
     }
 
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={() => setCurrentPage(Page.Register)}>Register</button>
-            <button onClick={() => setCurrentPage(Page.ForgotPassword)}>Forgot Password</button>
-            <button onClick={loginOnClick}>Login</button>
+        <div className="login">
+            <div className="login-container w-100 m-auto">
+                <form>
+                    <img className="mb-4 center" src={logoImage} width="75" height="65" />
+                    <h1 className="h3 mb-3 fw-normal text-center">התחברו ודרגו!</h1>
+
+                    {message && <span>
+                        <div className="alert alert-danger d-flex align-items-center" role="alert">
+                            {message}
+                        </div>
+                    </span>}
+                    <label className='form-label d-flex text-right mb-1'>אימייל</label>
+                    <input
+                        type="email"
+                        className="form-control left"
+                        placeholder="name@example.ariel.ac.il"
+                        value={username}
+                        onChange={evt => setUsername(evt.target.value)}
+                    />
+                    <label className='form-label d-flex text-right mt-2 mb-1'>סיסמא</label>
+                    <input
+                        type="password"
+                        className="form-control left"
+                        placeholder="Password"
+                        value={password}
+                        onChange={evt => setPassword(evt.target.value)}
+                    />
+
+                    <button className="btn btn-primary w-100 py-2" onClick={loginOnClick}>התחברות</button>
+                    <div className="text-center text-muted mt-5 mb-0">שכחת את הסיסמא?
+                        <button className="fw-bold text-body" onClick={() => setCurrentPage(Page.ForgotPassword)}>
+                            <u>איפוס הסיסמא</u>
+                        </button>
+                    </div>
+                </form>
+                <p className="text-center text-muted mt-5 mb-0">עדיין לא נרשמת?
+                    <button className="fw-bold text-body" onClick={() => setCurrentPage(Page.Register)}>
+                        <u>הרשמה כאן!</u>
+                    </button>
+                </p>
+            </div>
         </div>
     );
 }
